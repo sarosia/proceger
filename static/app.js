d06f5fae87b18ab351297b448c7f4aedc00f0367
sarosia <sarosia@users.noreply.github.com>
2021-02-07 01:01:24 -0800

    Rewrite UI using UIKit.

--- static/app.js
+++ static/app.js
@@ -1,27 +1,70 @@
-$ = function(id) {
-  return document.getElementById(id);
+const $ = function(id, type, attributes = {}, children = []) {
+  if (type === undefined) {
+    return document.getElementById(id);
+  }
+  const elm = document.createElement(type);
+  for (const [key, value] of Object.entries(attributes)) {
+    elm.setAttribute(key, value);
+  }
+  if (typeof children === 'string') {
+    elm.innerHTML = children;
+  } else {
+    for (const child of children) {
+      elm.appendChild(child);
+    }
+  }
+  if (id) {
+    if (typeof id === 'string') {
+      $(id).appendChild(elm);
+    } else {
+      id.appendChild(elm);
+    }
+  }
+  return elm;
+};
+
+$$ = function (type, attributes, children) {
+  return $(null, type, attributes, children);
 };
 
 window.onload = async function() {
   const res = await fetch('/task/list');
   const tasks = await res.json();
   for (const task of tasks) {
-    const div = document.createElement('div');
-    let innerHTML = task.name + ' ' + task.pid;
-    for (const [, logs] of Object.entries(task.logs)) {
-      innerHTML += '<div class="logs">';
-      for (const line of logs.split('\n')) {
-        try {
-          const log = JSON.parse(line);
-          console.log(log.timestamp, log.message);
-          innerHTML += `<p>${log.timestamp} ${log.message}</p>`;
-        } catch (e) {
-          // ignore
-        }
-      }
-      innerHTML += '</div>';
+    $('tasks-tab', 'li', {}, [$$('a', {'href':''}, task.name)]);
+    const content = $('tasks-content', 'li', {}, [
+      $$('div', {}, [
+        $$('ul', {'class': 'uk-list'}, [
+          $$('li', {}, `PID: ${task.pid}`),
+          $$('li', {}, `Repository: ${task.git}`),
+          $$('li', {}, `Revision: ${task.revision}`),
+          $$('li', {}, `Start time: ${new Date(task.startTime)}`),
+        ]),
+      ]),
+      $$('ul', {
+        'id': `task-${task.name}-logs-tab`,
+        'class': 'uk-tab',
+        'data-uk-tab': `{connect:'#task-${task.name}-logs'}`
+      }),
+      $$('ul', {
+        'id': `task-${task.name}-logs`,
+        'class': 'uk-switcher uk-margin',
+      }),
+    ]);
+    for (const [filename, logs] of Object.entries(task.logs)) {
+      $(`task-${task.name}-logs-tab`, 'li', {}, [
+        $$('a', {'href': ''}, filename),
+      ]);
+      $(`task-${task.name}-logs`, 'li', {}, [
+        $$('div', {'class': 'logs'}, logs.split('\n').map((line) => {
+          try {
+            const log = JSON.parse(line);
+            return $$('p', {}, `${log.timestamp} ${log.message}`);
+          } catch (e) {
+            return $$('p', {}, '');
+          }
+        })),
+      ]);
     }
-    div.innerHTML = innerHTML;
-    $('tasks').appendChild(div);
   }
 };
