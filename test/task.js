777ce1ded9632b7c9f6f9816bc00b10a35144558
sarosia <sarosia@users.noreply.github.com>
2021-02-10 23:30:44 -0800

    Fix lint errors.

--- test/task.js
+++ test/task.js
@@ -17,16 +17,16 @@ describe('Task', () => {
   });
 
   it('start then stop', async () => {
-    const task = new Task({ name: "repo1"}, git); 
+    const task = new Task({name: 'repo1'}, git);
     let json = await task.toJson();
     json.should.deep.equal({
-      "git": 'git@github.com:sarosia/repo1.git',
-      "logs": {},
-      "name": "repo1",
-      "path": repo1Path,
-      "pid": -1,
-      "revision": null,
-      "startTime": null,
+      'git': 'git@github.com:sarosia/repo1.git',
+      'logs': {},
+      'name': 'repo1',
+      'path': repo1Path,
+      'pid': -1,
+      'revision': null,
+      'startTime': null,
       'status': 'IDLE',
       'code': null,
     });
@@ -42,12 +42,12 @@ describe('Task', () => {
       json.startTime.should.not.equal(null);
       delete json['startTime'];
       json.should.deep.equal({
-        "git": 'git@github.com:sarosia/repo1.git',
-        "logs": {},
-        "name": "repo1",
-        "path": repo1Path,
-        "pid": -1,
-        "revision": null,
+        'git': 'git@github.com:sarosia/repo1.git',
+        'logs': {},
+        'name': 'repo1',
+        'path': repo1Path,
+        'pid': -1,
+        'revision': null,
         'status': 'STOPPED',
         'code': 'SIGTERM',
       });
@@ -55,7 +55,7 @@ describe('Task', () => {
   });
 
   it('restart', async () => {
-    const task = new Task({ name: "repo1"}, git); 
+    const task = new Task({name: 'repo1'}, git);
     try {
       await task.start();
       let json = await task.toJson();
